565fbc40dff2daf0393f173a7463ed1374ae3f16
Moskvin Andrey <berkold90@gmail.com>
2012-06-30 14:43:37 +0300

    Project setup, added cocoa pods

---
+++ morpheus/morpheus/DBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DBAppDelegate.h
+//  morpheus
+//
+//  Created by Moskvin Andrey on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
