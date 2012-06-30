19d696b0aee4121a9a621980ddc51f280ca82b1c
Moskvin Andrey <berkold90@gmail.com>
2012-06-30 17:53:18 +0300

    Added file guard and constants file

--- morpheus/morpheus/DBAppDelegate.m
+++ morpheus/morpheus/DBAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "DBAppDelegate.h"
+#import "DBFileWorker.h"
 
 @implementation DBAppDelegate
 
@@ -14,6 +15,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    DBFileWorker* worker = [[DBFileWorker alloc] init];
+    [worker testReadWriteCallbacks];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
