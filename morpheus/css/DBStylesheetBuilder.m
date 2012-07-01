5fdfcf2976be6dfb0e92f9126b6e28db661fc5b6
Moskvin Andrey <berkold90@gmail.com>
2012-07-01 13:09:46 +0300

    Added parsing of ui properties

--- morpheus/css/DBStylesheetBuilder.m
+++ morpheus/css/DBStylesheetBuilder.m
@@ -47,11 +47,23 @@
     dispatch_once(&onceToken, ^{
         NSMutableDictionary* aliases = [NSMutableDictionary dictionary];
         
+        // UIView
         [aliases setObject:@"layer.borderColor" forKey:@"border-color"];
         [aliases setObject:@"layer.borderWidth" forKey:@"border-width"];
         [aliases setObject:@"backgroundColor" forKey:@"background-color"];
         [aliases setObject:@"layer.cornerRadius" forKey:@"border-radius"];
         [aliases setObject:@"alpha" forKey:@"opacity"];
+        
+        // UILabel
+        [aliases setObject:@"textColor" forKey:@"color"];
+//        [aliases setObject:@"font" forKey:@"font-size"];
+//        [aliases setObject:@"font" forKey:@"font-name"];
+        [aliases setObject:@"textAlignment" forKey:@"text-align"];
+        [aliases setObject:@"lineBreakMode" forKey:@"line-break-mode"];
+        [aliases setObject:@"numberOfLines" forKey:@"number-of-lines"];
+        
+        // UINavigationBar
+        [aliases setObject:@"tintColor" forKey:@"tint-color"];
         
         result = [aliases copy];
     });
@@ -67,10 +79,13 @@
         NSMutableDictionary* types = [NSMutableDictionary dictionary];
         
         [types setObject:@"CGColor" forKey:@"layer.borderColor"];
-        [types setObject:@"NSInterger" forKey:@"layer.borderWidth"];
+        [types setObject:@"NSInteger" forKey:@"layer.borderWidth"];
         [types setObject:@"UIColor" forKey:@"backgroundColor"];
         [types setObject:@"CGFloat" forKey:@"layer.cornerRadius"];
         [types setObject:@"CGFloat" forKey:@"alpha"];
+        [types setObject:@"UITextAlignment" forKey:@"textAlignment"];
+        [types setObject:@"UILineBreakMode" forKey:@"lineBreakMode"];
+        [types setObject:@"NSInteger" forKey:@"number-of-lines"];
         
         result = [types copy];
     });
@@ -87,8 +102,10 @@
         
         [result setObject:NSStringFromSelector(@selector(parse_CGColor:)) forKey:@"CGColor"];
         [result setObject:NSStringFromSelector(@selector(parse_UIColor:)) forKey:@"UIColor"];
-        [result setObject:NSStringFromSelector(@selector(parse_NSNumber:)) forKey:@"NSInteger"];
-        [result setObject:NSStringFromSelector(@selector(parse_CGFloat)) forKey:@"CGFloat"];
+        [result setObject:NSStringFromSelector(@selector(parse_NSInteger:)) forKey:@"NSInteger"];
+        [result setObject:NSStringFromSelector(@selector(parse_CGFloat:)) forKey:@"CGFloat"];
+        [result setObject:NSStringFromSelector(@selector(parse_UITextAlignment:)) forKey:@"UITextAlignment"];
+        [result setObject:NSStringFromSelector(@selector(parse_UILineBreakMode:)) forKey:@"UILineBreakMode"];
         
         methods = [result copy];
     });
@@ -120,6 +137,62 @@
     return [NSNumber numberWithInt:[value integerValue]];
 }
 
+- (NSNumber *)parse_CGFloat:(NSString *)value
+{
+    return [NSNumber numberWithFloat:[value floatValue]];
+}
+
+-(NSValue *)parse_UITextAlignment:(NSString *)value
+{
+    if ([value isEqualToString:@"left"]) 
+    {
+        return [NSNumber numberWithInt:UITextAlignmentLeft];
+    }
+    else if ([value isEqualToString:@"right"]) 
+    {
+        return [NSNumber numberWithInt:UITextAlignmentRight];
+    }
+    else  if ([value isEqualToString:@"center"])
+    {
+        return [NSNumber numberWithInt:UITextAlignmentCenter];
+    }
+
+    NSAssert(NO,@"Wrong value: %@", value);
+    return nil;
+}
+
+- (NSValue *)parse_UILineBreakMode:(NSString *)value
+{
+    if ([value isEqualToString:@"word-wrap"])
+    {
+        return [NSNumber numberWithInt:UILineBreakModeWordWrap];
+    }
+    else if ([value isEqualToString:@"character-wrap"])
+    {
+        return [NSNumber numberWithInt:UILineBreakModeCharacterWrap];        
+    }
+    else if ([value isEqualToString:@"clip"])
+    {
+        return [NSNumber numberWithInt:UILineBreakModeClip];
+    }
+    else if ([value isEqualToString:@"head-truncation"])
+    {
+        return [NSNumber numberWithInt:UILineBreakModeHeadTruncation];        
+    }
+    else if ([value isEqualToString:@"tail-truncation"])
+    {
+        return [NSNumber numberWithInt:UILineBreakModeTailTruncation];   
+    }
+    else if ([value isEqualToString:@"middle-truncation"])
+    {
+        return [NSNumber numberWithInt:UILineBreakModeMiddleTruncation];
+    }    
+    
+    NSAssert(NO,@"Wrong value: %@", value);
+    return nil;
+}
+
+
 - (void)setSelector:(NSString *)selector
 {
     self.sheetSelector = selector;
@@ -138,6 +211,7 @@
     {
         NSString* methodName = [self.methods objectForKey:type];
         SEL selector = NSSelectorFromString(methodName);
+        NSAssert(selector != NULL,@"Unknown selector");
         value = [self performSelector:selector withObject:value];
     }
     
